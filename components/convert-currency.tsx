2a4b41fa6a897cd398ec4d59817b7303db21e7fb
Benelini <eli.bencur@gmail.com>
2024-01-27 14:18:43 +0100

    feat: new convert button

--- components/convert-currency.tsx
+++ components/convert-currency.tsx
@@ -5,6 +5,7 @@ import ExchangeIcon from "./svgs/exchange-icon";
 import { fetchExchangeRates } from "@/app/utils/fetch-exchange-rates";
 import { useQuery } from "@tanstack/react-query";
 import { ChangeEvent, useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 export const ConvertCurrency = () => {
   const [amount, setAmount] = useState(0);
@@ -24,10 +25,10 @@ export const ConvertCurrency = () => {
     return <div>Error: {error.message}</div>;
   }
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-end">
       {" "}
-      <div className="flex flex-row items-center justify-center">
-        <div className="flex flex-col justify-start items-start mx-4">
+      <div className="flex flex-row items-center justify-center gap-5">
+        <div className="flex flex-col justify-start items-start">
           <Input
             id="amount"
             placeholder="Enter currency amount"
@@ -38,16 +39,16 @@ export const ConvertCurrency = () => {
           />
         </div>
         <Combobox mainText="From..." value={from} setValue={setFrom} />
-        <button
-          className="px-4"
-          onClick={() => {
-            refetch();
-          }}
-        >
-          <ExchangeIcon className="w-10 h-10 fill-white hover:fill-slate-400" />
-        </button>
         <Combobox mainText="To..." value={to} setValue={setTo} />
       </div>
+      <Button
+        className="bg-white text-black mt-5"
+        onClick={() => {
+          refetch();
+        }}
+      >
+        Convert
+      </Button>
       {data ? (
         <div className="bg-white file:text-sm rounded-md py-2 mt-10 px-5 text-4xl ">
           {data.amount} {""}
