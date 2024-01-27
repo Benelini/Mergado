04c78cb51ecfea2b1f1b4a44afc9e0f12f3dcc93
Benelini <eli.bencur@gmail.com>
2024-01-27 13:59:32 +0100

    feat: fetch exchange rates function

---
+++ app/utils/fetch-exchange-rates.ts
@@ -0,0 +1,26 @@
+"use server";
+import axios from "axios";
+
+export type TExchangeRates =  {
+  amount: number;
+  from: string;
+  to: string;
+  timestamp: number;
+  base: string;
+  date: string;
+  success: boolean;
+  value: number;
+
+}
+
+export type TFetchExchangeRatesArgs = {
+  from: string;
+  to: string;
+  amount: number | string;
+}
+export const fetchExchangeRates = async ({ from, to, amount } : TFetchExchangeRatesArgs ): Promise<TExchangeRates> => {
+  const apiKey = process.env.API_KEY;
+  const url = `https://api.currencybeacon.com/v1/convert?from=${from.toUpperCase()}&to=${to.toUpperCase()}&amount=${amount}&api_key=${apiKey}`;
+  const response = await axios.get<TExchangeRates>(url);
+  return response.data;
+};
