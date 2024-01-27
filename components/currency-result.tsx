ad67a88863d2a8e356c3ce69cb19144db818f8ec
Benelini <eli.bencur@gmail.com>
2024-01-27 14:53:48 +0100

    fix: result of currency conversion format

--- components/currency-result.tsx
+++ components/currency-result.tsx
@@ -8,7 +8,7 @@ export const CurrencyResult = ({ data }: CurrencyResultProps) => {
   return (
     <div className="bg-white self-center max-md:text-lg file:text-sm rounded-md py-2 mt-10 px-5 text-4xl ">
       {data.amount} {""}
-      {data.from} = {data.value.toPrecision(3)} {data.to}
+      {data.from} = {data.value.toFixed(3)} {data.to}
     </div>
   );
 };
