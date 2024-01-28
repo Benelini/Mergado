30f8b02f4d675e8e8447d1a23cd0c62f528e9d6f
Benelini <eli.bencur@gmail.com>
2024-01-28 10:54:23 +0100

    refactor: throw error if api key is not set

--- actions/fetch-conversion.ts
+++ actions/fetch-conversion.ts
@@ -19,6 +19,9 @@ export type TFetchConversionArgs = {
 }
 export const fetchConversion = async ({ from, to, amount } : TFetchConversionArgs ): Promise<TConversion> => {
   const apiKey = process.env.API_KEY;
+  if (!apiKey) {
+    throw new Error('API key is not set');
+  }
   const url = `https://api.currencybeacon.com/v1/convert?from=${from.toUpperCase()}&to=${to.toUpperCase()}&amount=${amount}&api_key=${apiKey}`;
   const response = await axios.get<TConversion>(url);
   return response.data;
